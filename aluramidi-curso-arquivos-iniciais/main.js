c4f7c81a316ec31981ce7b48f64a49eef164d419
E. H. C. G <114739362+iodoGuduD@users.noreply.github.com>
2023-09-13 11:40:56 -0300

    Lógica roteirizada

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,11 @@
-document.querySelector();
+//1° listar todos os buttons
+//2° listar todos os áudios
+//3° ouvir o click em todos os buttons
+//4° identificar qual button é e seu respectivo som através do id e tag
+//5° tocar o respectivo som
+
+for(i=0; i < buttons.lenght; i++){
+
+};
+
+document.querySelector('#som_tecla_pom').play();
