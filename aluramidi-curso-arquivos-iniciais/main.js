6b8c4eb17b4c042a5fe9da4b4701184950f4a00a
E. H. C. G <114739362+iodoGuduD@users.noreply.github.com>
2023-09-20 11:32:58 -0300

    Avançando nas funções e script defer

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,10 +1,18 @@
-//1° listar todos os buttons
-//2° listar todos os áudios
+//1° listar todos os buttons, posso tentar um for que cria um array pra mim
+//2° listar todos os áudios, mesma coisa de cima
 //3° ouvir o click em todos os buttons
 //4° identificar qual button é e seu respectivo som através do id e tag
 //5° tocar o respectivo som
+let buttons = [];
+let sounds = [];
 
-for(i=0; i < buttons.lenght; i++){
+for(i=0; i <= 9; i++){
+    
+
+    return buttons;
+};
+
+for(i=0; i <= buttons.length; i++){
 
 };
 
